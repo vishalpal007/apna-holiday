2d54f9bfc38ab226939a26ab7df85b34329c09cb
vishal <vp461365@gmail.com>
2024-03-11 17:45:10 +0530

    new commit updatelast

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.static("uploads"))
 app.use(cookieParser())
 
-app.use(express.static(path.join(__dirname, "..", "dist")))
+app.use(express.static(path.join(__dirname, "dist")))
 
 app.use(cors({
     origin: "https://apna-holiday.onrender.com/",
